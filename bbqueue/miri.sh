fbbb3ef3a125e0b99cc794acd300d2edb34ff550
Rhett Griggs <rhett@cloudflare.com>
2026-07-30 18:31:31 -0400

    Enable Miri leak checking
    The existing Arc-backed tests expose owned handle leaks, but the Miri
    script disabled leak reports. Keep the checker enabled now that the
    suite is leak-free so CI catches future regressions.

--- bbqueue/miri.sh
+++ bbqueue/miri.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # We disable isolation because we use tokio sleep
-# We disable leaks because ???
-#
-# TODO: Can we eliminate some of these limitations for testing?
-MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-ignore-leaks" \
+MIRIFLAGS="-Zmiri-disable-isolation" \
     cargo +nightly miri test \
     --target x86_64-unknown-linux-gnu \
     --features=std
